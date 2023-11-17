13c3272f941a99286ad7bc82821976866ac0cdf4
Feylor <riki.ambrella@gmail.com>
2023-11-17 18:17:57 +0500

    Fix 100 on 1000

--- src/main/java/ru/nikita/tasks/TaskB.java
+++ src/main/java/ru/nikita/tasks/TaskB.java
@@ -2,7 +2,7 @@ package ru.nikita.tasks;
 
 public class TaskB {
     public static void execute(){
-        int a = 100;
+        int a = 1000;
         long b = 1_000_000;
         double pi = 3.14159;
         float f = 2.5f;
