b52060488112453dfe73cad3715dd33233ac47ca
Feylor <riki.ambrella@gmail.com>
2023-12-06 16:43:43 +0500

    Add TAsk

--- src/main/java/ru/nikita/tasks/OperTask8.java
+++ src/main/java/ru/nikita/tasks/OperTask8.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class OperTask8 {
     public static void main(String[] args) {
         Random r = new Random();
-        int n = r.nextInt(6);
+        int n = r.nextInt(8);
         int x = 1;
 
         for (int i = 1; i <= n; i++) {
