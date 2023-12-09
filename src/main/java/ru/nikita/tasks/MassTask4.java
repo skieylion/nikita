324fa2d9bd54f35fee172cf6a9a3cf7610aea46a
Feylor <riki.ambrella@gmail.com>
2023-12-09 19:01:33 +0500

    Add Task Massive

--- src/main/java/ru/nikita/tasks/MassTask4.java
+++ src/main/java/ru/nikita/tasks/MassTask4.java
@@ -2,14 +2,14 @@ package ru.nikita.tasks;
 
 public class MassTask4 {
     public static void main(String[] args) {
-        char[][] saw = {
+        char[][] May = {
                 {'a', 'b'},
                 {'c', 'd'},
                 {'e', 'f'},
                 {'g', 'o'}};
-        for (char[] sew : saw) {
-            for (char siw : sew) {
-                System.out.print(siw + " ");
+        for (char[] Rick : May) {
+            for (char Sam : Rick) {
+                System.out.print(Sam + " ");
             }
             System.out.println();
         }
--- src/main/java/ru/nikita/tasks/MassTask5.java
+++ src/main/java/ru/nikita/tasks/MassTask5.java
@@ -5,27 +5,28 @@ import java.util.Optional;
 
 public class MassTask5 {
     public static void main(String[] args) {
-        int[][] MasMay = new int[5][8];
-        for (int i = 0; i < MasMay.length; i++) {
-            for (int j = 0; j < MasMay[i].length; j++) {
-                MasMay[i][j] = (i) + (j + 1);
-                System.out.print(MasMay[i][j] + " ");
+        int[][] array5x8 = {
+                {3, 2, 5, 6, 44, 43, 1, 4},
+                {5, 22, 11, 61, 46, 40, 1, 7},
+                {1, 21, 23, 62, 13, 47, 2, 8},
+                {9, 29, 41, 63, 15, 75, 3, 12},
+                {0, 28, 52, 64, 10, 71, 4, 11}
+        };
+        int[][] arrayOfMinAndMaxValues = new int[5][2];
 
+        for (int i = 0; i < array5x8.length; i++) {
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < array5x8[i].length; j++) {
+                max = Math.max(max, array5x8[i][j]);
+                min = Math.min(min, array5x8[i][j]);
             }
-            System.out.println();
+            arrayOfMinAndMaxValues[i] = new int[]{max, min};
+            System.out.println(max + " " + min + " ");
         }
 
-        System.out.println();
-
-        int[][] May = new int[5][2];
-        int[] MinV = Arrays.stream(MasMay)
-                .mapToInt(row -> Arrays.stream(row).min().orElse(0))
-                .toArray();
-        int[] MaxV = Arrays.stream(MasMay)
-                .mapToInt(row -> Arrays.stream(row).max().orElse(0))
-                .toArray();
-        System.out.println(Arrays.toString(MinV) + " - MinValue");
-        System.out.println(Arrays.toString(MaxV) + " - MaxValue");
     }
+
+
 }
 
