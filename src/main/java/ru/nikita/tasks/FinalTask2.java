59692eb9045281516a1caa3e0fa28c9c6c777b96
Feylor <riki.ambrella@gmail.com>
2023-12-13 17:27:51 +0500

    Add FinalTasks

---
+++ src/main/java/ru/nikita/tasks/FinalTask2.java
@@ -0,0 +1,26 @@
+package ru.nikita.tasks;
+
+public class FinalTask2 {
+    public static void main(String[] args) {
+        class Solution {
+            public void merge(int[] nums1, int m, int[] nums2, int n) {
+                int offset = m;
+                lable:
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < offset; j++) {
+                        if (nums1[j] > nums2[i]) {
+                            for (int k = nums1.length - 1; k > j; k--) {
+                                nums1[k] = nums1[k - 1];
+                            }
+                            nums1[j] = nums2[i];
+                            offset++;
+                            continue lable;
+                        }
+
+                    }
+                    nums1[offset++] = nums2[i];
+                }
+            }
+        }
+    }
+}
