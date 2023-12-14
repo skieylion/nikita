3f611b8a38fd71a4ba0ca7709fa7d16662fdf705
Feylor <riki.ambrella@gmail.com>
2023-12-14 19:25:30 +0500

    Add ClassCar

---
+++ src/main/java/ru/nikita/tasks/classCar.java
@@ -0,0 +1,31 @@
+package ru.nikita.tasks;
+
+public class classCar {
+    String colour;
+
+    String number;
+
+    int modele;
+
+    int speed;
+
+    void Ride() {
+    }
+
+    void Stop() {
+    }
+
+    void setSpeed() {
+
+    }
+}
+class motor {
+    double volume;
+    int power;
+    int cost;
+
+    void Start() {
+
+    }
+
+}
