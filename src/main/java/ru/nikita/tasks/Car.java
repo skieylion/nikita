3f57216fb9e631e73e45b4dfe3fbd13148b55085
Feylor <riki.ambrella@gmail.com>
2023-12-17 20:38:06 +0500

    Update ClassCar 2

--- src/main/java/ru/nikita/tasks/Car.java
+++ src/main/java/ru/nikita/tasks/Car.java
@@ -2,37 +2,49 @@ package ru.nikita.tasks;
 
 public class Car {
 
-    Engine engine = new Engine();
 
+    Engine engine;
     String colour;
     String number;
     String model;
     int speed;
+
+    Car() {
+        engine = new Engine();
+    }
+
     void Ride() {
     }
-    String ModelCar() {
+
+    String modelCar() {
         model = model + " - " + engine.volume;
         return model;
     }
-    void StartOrStop() {
+
+
+    void Start() {
         if (engine.mode == true) {
             System.out.println("Машина завелась");
-        } else {
+        }
+    }
+
+    void Stop() {
+        if (engine.mode == false) {
             System.out.println("Машина не завелась");
         }
     }
-    void setSpeed() {
-        if (speed <= 200) {
-            System.out.println();
-        } else {
-            while (speed > 200) {
-                speed--;
+
+    void setSpeed(int s) {
+        s = 140;
+        if (s < 200 && s >= 0) {
+            System.out.println("Скорость в норме");
+        } else  {
+            System.out.println("Speed error");
             }
         }
     }
-}
-class Engine {
 
+class Engine {
 
 
     boolean mode;
@@ -42,12 +54,12 @@ class Engine {
     int cost; // Затраты двигателя n км/л
 
 
-    void Start() {
-        mode = true;
-    }
-
-    void Stop() {
-        mode = false;
+    void setMode() {
+        if (mode == true) {
+            System.out.println("Двигатель запущен");
+        } else if (mode == false) {
+            System.out.println("Двигатель остановлен");
+        }
     }
 
 }
