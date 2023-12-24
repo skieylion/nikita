b4709101e7d3599f49290cf7a3f8d0cbab145463
Feylor <riki.ambrella@gmail.com>
2023-12-24 17:38:17 +0500

    Update Circle and Square 1

--- src/main/java/ru/nikita/tasks/Circle.java
+++ src/main/java/ru/nikita/tasks/Circle.java
@@ -1,11 +1,15 @@
 package ru.nikita.tasks;
 
+import java.util.Scanner;
+
 public class Circle {
     public static final double Pi = 3.14;
 
-    static double radius;
+    static Double radius;
 
     double getArea () {
+        Scanner scanner = new Scanner(System.in);
+        radius = scanner.nextDouble();
         return  Pi * radius * radius;
     }
 }
