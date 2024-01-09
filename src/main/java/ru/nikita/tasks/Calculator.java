db3c3d78d9a2a0470b38c24318e4742f282e77aa
Feylor <riki.ambrella@gmail.com>
2024-01-09 16:06:56 +0500

    Create Calculator

--- src/main/java/ru/nikita/tasks/Calculator.java
+++ src/main/java/ru/nikita/tasks/Calculator.java
@@ -3,19 +3,37 @@ package ru.nikita.tasks;
 public class Calculator {
 
 
-    public void add(int x, int y, int... args) {
+
+    public int add(int x, int y, int... args) {
+        int result = x + y;
+        for (int v : args) {
+            result += v;
+        }
+        return result;
     }
 
-    public void substruct(int x, int y, int... args) {
-
+    public int substruct(int x, int y, int... args) {
+        int result = x - y;
+        for (int v : args) {
+            result -= v;
+        }
+        return result;
     }
 
-    public void multiply(int x, int y, int... args) {
-
+    public int multiply(int x, int y, int... args) {
+        int result = x * y;
+        for (int v : args) {
+            result *= v;
+        }
+        return result;
     }
 
-    public void divide(int x, int y, int... args) {
-
+    public int divide(int x, int y, int... args) {
+        int result = x / y;
+        for (int v : args) {
+            result /= v;
+        }
+        return result;
     }
 
 }
