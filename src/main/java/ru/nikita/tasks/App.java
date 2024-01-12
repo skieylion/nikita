cb6ee6008ea1ee969f919d9cb439551c53f8dfdf
Feylor <riki.ambrella@gmail.com>
2024-01-12 13:49:38 +0500

    Create Time

--- src/main/java/ru/nikita/tasks/App.java
+++ src/main/java/ru/nikita/tasks/App.java
@@ -1,16 +1,9 @@
 package ru.nikita.tasks;
 
-import java.util.Random;
-import java.util.Scanner;
-
 import static java.awt.AWTEventMulticaster.add;
 
 public class App {
     public static void main(String[] args) {
-        // Scanner scanner = new Scanner(System.in);
-        // double radius = scanner.nextDouble();
-
-
         /* Random random = new Random();
         Square square1 = new Square((int) (random.nextDouble() * 100));
         Square square2 = new Square((int) (random.nextDouble() * 100));
@@ -18,17 +11,23 @@ public class App {
         Cube cube1 = Cube.cubeOfSquare(square1);
         Cube cube2 = Cube.cubeOfSquare(square2);
         System.out.println();
-        */
-
         Calculator calculator = new Calculator();
-
         System.out.println("Сложение - " + calculator.add(1, 2, 3, 4, 5, 6, 7));
         System.out.println("Вычетание - " + calculator.substruct(1, 2, 3, 4, 5, 6, 7));
         System.out.println("Умножение - " + calculator.multiply(1, 2, 3, 4, 5, 6, 7));
         System.out.println("Деление - " + calculator.divide(512, 2, 2, 2, 2, 2, 2));
+        */
 
+        show();
+    }
 
-
+    public static void show() {
+        Time time1 = new Time(23, 22, 13);
+        System.out.println(time1.hour + ":" + time1.minutes + ":" + time1.second);
+        Time time2 = new Time(23, 22);
+        System.out.println(time2.hour + ":" + time2.minutes + ":0" + time2.second);
+        Time time3 = new Time(22);
+        System.out.println(time3.hour + ":" + time3.minutes + "0:0" + time3.second);
     }
 
 
---
+++ src/main/java/ru/nikita/tasks/Time.java
@@ -0,0 +1,29 @@
+package ru.nikita.tasks;
+
+public class Time {
+
+    int hour;
+    int minutes;
+    int second;
+
+    Time(int h, int m, int s) {
+        this.hour = h;
+        this.minutes = m;
+        this.second = s;
+    }
+
+    Time(int h, int m) {
+        this.hour = h;
+        this.minutes = m;
+    }
+
+    Time(int h) {
+        this.hour = h;
+    }
+
+    public void plus(int h, int m, int s) {
+
+
+    }
+
+}
