f5168aee32a3c87b069e01ef5c163b5e4fc7de16
Feylor <riki.ambrella@gmail.com>
2024-02-02 18:15:51 +0500

    Create Task1

--- src/main/java/ru/nikita/tasks/App.java
+++ src/main/java/ru/nikita/tasks/App.java
@@ -5,21 +5,16 @@ import static java.awt.AWTEventMulticaster.add;
 public class App {
     public static void main(String[] args) {
 
-        Client client = new Client();
-
-        client.setAge(16);
-        client.setName("Ваня");
-        client.setProfession(" -- ");
-        client.getFullName();
-
-        Employee employee = new Employee();
-
-        employee.setAge(25);
-        employee.setName("Александр");
-        employee.setProfession(" Официант ");
-        employee.setExperience(3);
-        employee.getFullName();
+        plus(10);
 
     }
+
+    public static void plus (int x) {
+        x += 10;
+        System.out.println(x);
+    }
+
+
+
 
 }
